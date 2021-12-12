5202016d8a435335d08cce14453416bc37ccfd43
liffrey <bugrauvez@gmail.com>
2021-12-12 16:47:02 +0300

    first_version

---
+++ function_templates.cpp
@@ -0,0 +1,23 @@
+//example code got from cplusplus.com/doc/tutorials/function2/
+//function template
+#include <iostream>
+
+template <class T>
+
+T sum (T a, T b)
+{
+    T result;
+    result = a+b;
+    return result;
+}
+
+int main()
+{
+    int i=5, j=6, k;
+    double f=2.0,g=5.4,h;
+    k=sum<int>(i,j);
+    h=sum<double>(f,g);
+    std::cout<<k<<'\n';
+    std::cout<<h<<'\n';
+    return 0;
+}
